193e3e28dea9f88861bc789e74c96bd5c1c520c9
imochoa <nachomartinochoa@gmail.com>
2021-04-08 12:54:29 +0200

    added URL to spotify snap high-dpi scaling issue fix

--- index/spotify/install.sh
+++ index/spotify/install.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 sudo snap install --classic spotify
+# High DPI scaling issue fix:
+# https://community.spotify.com/t5/Desktop-Linux/Spotify-Hi-DPI-Fix-for-Snap-install/td-p/4576328
