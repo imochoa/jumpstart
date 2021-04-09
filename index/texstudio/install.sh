348e3dc369b58477d532483e6305e26b57c7c77f
imochoa <nachomartinochoa@gmail.com>
2021-04-09 15:16:38 +0200

    extended the latex deps

--- index/texstudio/install.sh
+++ index/texstudio/install.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo apt install -y texstudio texlive-latex-extra texlive-xetex
+sudo apt install -y texstudio texlive-latex-extra texlive-xetex \
+&& sudo apt install -y texlive-fonts-extra texlive-full
+# Added the missing fonts
