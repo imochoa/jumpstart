7d1acbb0d1b84a2cb9c66374e64bd832df52ebc3
imochoa <nachomartinochoa@gmail.com>
2021-04-09 15:49:42 +0200

    added zinit update

--- index/zsh/update.sh
+++ index/zsh/update.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 cd ${HOME}/.zinit/bin && git pull
+
+# ZINIT!
+# You can simple self upgrade zinit with the following command:
+zinit self-update
+# Also you can update all the plugins with:
+zinit update
