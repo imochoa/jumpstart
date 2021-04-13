0922fdd77b8cc8bdb92e640d2c7f78cd8eb566be
imochoa <nachomartinochoa@gmail.com>
2021-04-13 14:54:36 +0200

    stretchly

--- index/stretchly/install.sh
+++ index/stretchly/install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-VER=$(git ls-remote --refs --tags https://github.com/hovancik/stretchly \
-    | cut --delimiter='/' --fields=3     \
-    | tr '-' '~'                         \
-    | sort --version-sort                \
-    | tail --lines=1);
-# e.g. VER=v1.2.0
-sudo rm -f /usr/local/bin/stretchly \
-&& sudo wget https://github.com/hovancik/stretchly/releases/download/${VER}/Stretchly-${VER:1}.AppImage \
-    --output-document=/usr/local/bin/stretchly \
-&& sudo chmod +x /usr/local/bin/stretchly;
+sudo snap install stretchly
+
+# VER=$(git ls-remote --refs --tags https://github.com/hovancik/stretchly \
+#     | cut --delimiter='/' --fields=3     \
+#     | tr '-' '~'                         \
+#     | sort --version-sort                \
+#     | tail --lines=1);
+# # e.g. VER=v1.2.0
+# sudo rm -f /usr/local/bin/stretchly \
+# && sudo wget https://github.com/hovancik/stretchly/releases/download/${VER}/Stretchly-${VER:1}.AppImage \
+#     --output-document=/usr/local/bin/stretchly \
+# && sudo chmod +x /usr/local/bin/stretchly;
