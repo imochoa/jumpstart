7de2a7a0e196764704d24ec6d43f45d517de5fb5
imochoa <nachomartinochoa@gmail.com>
2021-04-13 14:53:15 +0200

    added beekeeper

--- index/beekeeper-studio/install.sh
+++ index/beekeeper-studio/install.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
+sudo snap install beekeeper-studio
+
 # https://docs.beekeeperstudio.io/installation/#apt-deb
+# # Install our GPG key
+# wget --quiet -O - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
 
-# Install our GPG key
-wget --quiet -O - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+# # add our repo to your apt lists directory
+# echo "deb https://dl.bintray.com/beekeeper-studio/releases disco main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio.list
 
-# add our repo to your apt lists directory
-echo "deb https://dl.bintray.com/beekeeper-studio/releases disco main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio.list
+# # Update apt and install
+# sudo apt update
+# sudo apt install beekeeper-studio
 
-# Update apt and install
-sudo apt update
-sudo apt install beekeeper-studio
-
-# Installed to '/opt/' by default!
-BEEKEEPER_DIR="/opt/Beekeeper Studio"
-sudo chown -R ${USER}:${USER} "${BEEKEEPER_DIR}"
-sudo rm -f /usr/local/bin/beekeeper-studio
-ln -s ${BEEKEEPER_DIR}/beekeeper-studio /usr/local/bin/beekeeper-studio
-sudo ln -s "${BEEKEEPER_DIR}/beekeeper-studio" /usr/local/bin/beekeeper-studio
+# # Installed to '/opt/' by default!
+# BEEKEEPER_DIR="/opt/Beekeeper Studio"
+# sudo chown -R ${USER}:${USER} "${BEEKEEPER_DIR}"
+# sudo rm -f /usr/local/bin/beekeeper-studio
+# ln -s ${BEEKEEPER_DIR}/beekeeper-studio /usr/local/bin/beekeeper-studio
+# sudo ln -s "${BEEKEEPER_DIR}/beekeeper-studio" /usr/local/bin/beekeeper-studio
