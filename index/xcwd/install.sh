88de4b1d4846ff611991d2f389e486733f3c37c8
imochoa <nachomartinochoa@gmail.com>
2021-04-15 16:07:55 +0200

    added xcwd

--- index/xcwd/install.sh
+++ index/xcwd/install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-sudo apt install -y git \
-&& sudo mkdir -p /opt/ \
-&& cd /opt/ \
-&& sudo git clone https://github.com/schischi/xcwd.git xcwd \
-&& sudo chown -R ${USER}:${USER} /opt/xcwd \
-&& cd xcwd/ \
-&& make \
-&& sudo checkinstall
-# && sudo make install
+
+sudo apt install -y xcwd
+
+# sudo apt install -y git checkinstall                        \
+# && sudo mkdir -p /opt/                                      \
+# && cd /opt/                                                 \
+# && sudo git clone https://github.com/schischi/xcwd.git xcwd \
+# && sudo chown -R ${USER}:${USER} /opt/xcwd                  \
+# && cd xcwd/                                                 \
+# && make                                                     \
+# && sudo checkinstall
+# # && sudo make install
