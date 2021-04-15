ef3ae2d7f4fc8ef4c9d0da049e6acbf5f66a5ade
imochoa <nachomartinochoa@gmail.com>
2021-04-15 18:16:32 +0200

    nautilus plugins

---
+++ index/nautilus-plugins/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt install -y nautilus-extension-gnome-terminal nautilus-actions
+
+# Run nautilus -q command to quit nautilus
+# nautilus -q
---
+++ index/nautilus-plugins/remove.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt remove -y nautilus-extension-gnome-terminal nautilus-actions
+
