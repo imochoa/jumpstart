8f78635cd86b3a86552857de8684804292828338
imochoa <nachomartinochoa@gmail.com>
2021-05-17 15:06:03 +0200

    materia-theme

---
+++ index/materia-theme/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Dependencies -> materia
+sudo apt install -y gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils \
+  && sudo apt install -y materia-gtk-theme
---
+++ index/materia-theme/remove.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo rm -rf /usr/share/themes/Materia{,-compact,-dark,-dark-compact,-light,-light-compact}
