05a2321fa27175f0f93240a002952338e43a5e34
imochoa <nachomartinochoa@gmail.com>
2021-05-18 16:41:23 +0200

    fixed duf

--- index/duf/install.sh
+++ index/duf/install.sh
@@ -8,7 +8,9 @@ VER=$(git ls-remote --refs --tags https://github.com/muesli/duf \
     | sort --version-sort                \
     | tail --lines=1);
 
-TMP_DEB=/tmp/duf.deb
+INSTALL_TEMPDIR=$(mktemp -d -t duf-XXXXXXXXXX)
+
+TMP_DEB="${INSTALL_TEMPDIR}/tmp/duf.deb"
 wget https://github.com/muesli/duf/releases/download/${VER}/duf_${VER:1}_linux_amd64.deb \
   --continue \
   --output-document=${TMP_DEB} \
