4ad5267ede983d28ba64fbf0a40e9f025e666879
imochoa <nachomartinochoa@gmail.com>
2021-05-26 11:11:06 +0200

    improved peazip

--- index/peazip/install.sh
+++ index/peazip/install.sh
@@ -7,13 +7,12 @@ VER=$(git ls-remote --refs --tags https://github.com/peazip/PeaZip \
     | tail --lines=1);
 URL="https://github.com/peazip/PeaZip/releases/download/${VER}/peazip_${VER}.LINUX.x86_64.GTK2.deb"
 
-# INSTALL_TEMPDIR=$(mktemp -d -t peazip-XXXXXXXXXX)
-# TMP_DEB="${INSTALL_TEMPDIR}/peazip.deb"
-TMP_DEB="/tmp/peazip.deb"
+INSTALL_TEMPDIR=$(mktemp -d -t peazip-XXXXXXXXXX)
+TMP_DEB="${INSTALL_TEMPDIR}/peazip.deb"
+# TMP_DEB="/tmp/peazip.deb"
 
-echo $VER
-wget "${URL}" \
-  --continue \
+wget "${URL}"                  \
+  --continue                   \
   --output-document=${TMP_DEB} \
 && sudo apt install ${TMP_DEB}
 
