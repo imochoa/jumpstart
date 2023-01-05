2e90e5cbd6e56f404354a20093666d5d5a4f9188
imochoa <nachomartinochoa@gmail.com>
2023-01-05 20:18:42 +0100

    - added more apt packages

---
+++ packages/neovim/flatpak/.post/install.sh
@@ -0,0 +1,9 @@
+# [[[cog
+# import cog
+# from cog_utils import str2list, require_cmds, chain_cmds, printf
+#
+# cog.outl(r"# TODO")
+# cog.outl(r"# https://github.com/neovim/neovim/wiki/Installing-Neovim#flatpak")
+#
+# ]]]
+# [[[end]]]
